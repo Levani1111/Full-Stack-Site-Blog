0328c0e28aafc43b0d6cb610a85c9ddb19cb8e56
Levani1111 <papashvililevani@gmail.com>
2024-04-17 22:38:42 +0200

    init

---
+++ my-blog-backend/src/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/Hello', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+app.listen(8000, () => {
+    console.log('Server is running on http://localhost:8000');
+});
