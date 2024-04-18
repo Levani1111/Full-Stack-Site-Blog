7f2087c85d76f8303bf25483cc7a3216fe04bb48
Levani1111 <papashvililevani@gmail.com>
2024-04-18 16:50:06 +0200

    Adding comments

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -34,6 +34,21 @@ app.put("/api/articles/:name/upvote", (req, res) => {
     }
 });
 
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+
+    const article = articlesInfo.find(a => a.name === name);
+
+    if (article) {
+        article.comments.push({ postedBy, text });
+        res.send(article.comments);
+    } else {
+        res.send('That article doesn\'t exist!');
+    }
+});
+
 app.listen(8000, () => {
     console.log("Server is running on http://localhost:8000");
 });
