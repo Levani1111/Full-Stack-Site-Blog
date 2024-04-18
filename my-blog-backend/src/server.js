d74fad515a2266d8e838c62daeacbe5954200352
Levani1111 <papashvililevani@gmail.com>
2024-04-18 16:20:13 +0200

    Upvoting articles

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,17 +1,39 @@
-import express from 'express';
+import express from "express";
+
+let articlesInfo = [
+    {
+        name: "learn-react",
+        upvotes: 0,
+        comments: [],
+    },
+    {
+        name: "learn-node",
+        upvotes: 0,
+        comments: [],
+    },
+    {
+        name: "mongodb",
+        upvotes: 0,
+        comments: [],
+    },
+];
 
 const app = express();
 app.use(express.json());
 
-app.post('/hello', (req, res) => {
-    res.send(`Hello ${req.body.name}!`);
-});
-
-app.get('/hello/:name', (req, res) => {
+app.put("/api/articles/:name/upvote", (req, res) => {
     const { name } = req.params;
-    res.send(`Hello ${name}!!`);
+
+    const article = articlesInfo.find((a) => a.name === name);
+
+    if (article) {
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+    } else {
+        res.send("That article doesn't exist");
+    }
 });
 
 app.listen(8000, () => {
-    console.log('Server is running on http://localhost:8000');
+    console.log("Server is running on http://localhost:8000");
 });
