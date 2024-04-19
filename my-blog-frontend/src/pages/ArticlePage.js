82850b1b910cb485943bfd5964473a382242de38
Levani1111 <papashvililevani@gmail.com>
2024-04-19 15:14:07 +0200

    Connecting Front End & Back End

--- my-blog-frontend/src/pages/ArticlePage.js
+++ my-blog-frontend/src/pages/ArticlePage.js
@@ -1,10 +1,27 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
     const { articleID } = useParams();
+
+
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleID}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        }
+        loadArticleInfo();
+    }, []);
+
+
     const article = articles.find((article) => article.name === articleID);
+
 
     if (!article) {
         return <NotFoundPage />;
@@ -13,6 +30,7 @@ const ArticlePage = () => {
     return (
         <>
             <h1>{article.title}</h1>
+            <p>This post has been upvoted {articleInfo.upvotes} times</p>
             {article.content.map((paragraph, i) => (
                 <p key={i}>{paragraph}</p>
             ))}
